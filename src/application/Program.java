51da30c3aafd36402924db2f090e13a85b262219
Raphael B <rapha18023@gmail.com>
2023-05-23 12:49:12 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
     }
 }
